62741a4d867b2ba211fec0bfa4eea76db7383bf9
Anaïs Bekolo <anaisbekolo@hotmail.fr>
2023-04-04 10:56:08 +0200

    ajout des lemmes + retrait du hard coding : ajout d'une variable pour définir la taille du contexte

--- brouillon.py
+++ brouillon.py
@@ -1,6 +1,24 @@
 import spacy
+import xml.etree.ElementTree as ET
+
 nlp = spacy.load('fr_core_news_md')
 
+#paths à définir
+data_path = "../donnees/FSE-1.1-191210/wiktionary-190418.data.xml"
+gold_path = "../donnees/FSE-1.1-191210/wiktionary-190418.gold.key.txt"
+
+#récupération des données XML
+tree = ET.parse(data_path)
+data = tree.getroot()[0]
+
+#récupération des données .txt
+gold_file = open(gold_path, "r",encoding="utf-8")
+
+for sentence in data[:5]:
+    for word in sentence :
+        print(word.attrib['lemma'])
+
+'''
 context_before = []
 context_after = []
 
@@ -31,9 +49,13 @@ for i in range(10-len(context_before)) :
 for i in range(10-len(context_after)) :
     context_after.append("<f>")
         
-print(context_before)
-print(context_after)
+#print(context_before)
+#print(context_after)
+'''
 
+'''
 for word in context_before :
     print(nlp(word))
     print(nlp(word)[0].lemma_)
+'''
+
--- extraction_donnees_entrainement.py
+++ extraction_donnees_entrainement.py
@@ -1,6 +1,5 @@
 import re
 import xml.etree.ElementTree as ET
-import spacy
 
 #paths à définir
 data_path = "donnees/FSE-1.1-191210/wiktionary-190418.data.xml"
@@ -13,12 +12,8 @@ data = tree.getroot()[0]
 #récupération des données .txt
 gold_file = open(gold_path, "r",encoding="utf-8")
 
-'''
-#lemmatizer
-nlp = spacy.load('fr_core_news_md')
-'''
-
 X = []
+context_size = 10
 
 for (sentence,gold_line) in zip(data,gold_file.readlines()) :
     
@@ -33,46 +28,40 @@ for (sentence,gold_line) in zip(data,gold_file.readlines()) :
     while sentence[i_instance].tag != "instance" : 
         i_instance+=1
         
-    context_vector["instance"] = sentence[i_instance].text.lower()
+    context_vector["instance"] = sentence[i_instance].attrib['lemma'].lower()
     
     #on vérifie la longueur des phrases pour ne pas soulever d'erreur
-    if (len(sentence[:i_instance])>=10) :
-            for i in range(1,11) :
-                context_before.append(sentence[i_instance-i].text.lower())
+    if (len(sentence[:i_instance])>=context_size) :
+            for i in range(1,context_size+1) :
+                context_before.append(sentence[i_instance-i].attrib['lemma'].lower())
     else :
         for i in range(1,len(sentence[:i_instance])+1) :
-            context_before.append(sentence[i_instance-i].text.lower())
+            context_before.append(sentence[i_instance-i].attrib['lemma'].lower())
 
-    if(len(sentence[i_instance+1:])>= 10) :
-        for i in range(i_instance+1,i_instance+11):
-            context_after.append(sentence[i].text.lower())
+    if(len(sentence[i_instance+1:])>= context_size) :
+        for i in range(i_instance+1,i_instance+(context_size+1)):
+            context_after.append(sentence[i].attrib['lemma'].lower())
     else :
         for i in range(i_instance+1,len(sentence)):
-            context_after.append(sentence[i].text.lower())
+            context_after.append(sentence[i].attrib['lemma'].lower())
     
     #une fois les listes constituées, on ajoute les balises de début et de fin de phrase si nécessaire
-    for i in range(10-len(context_before)) :
+    for i in range(context_size-len(context_before)) :
         context_before.append("<d>")
         
-    for i in range(10-len(context_after)) :
+    for i in range(context_size-len(context_after)) :
         context_after.append("<f>")
         
     #on construit le vecteur de contexte à partir des listes finalisées
-    for i in range(10):
+    for i in range(context_size):
         context_vector["next_word_"+str(i+1)] = context_after[i]
         
-    for i in range(10):
+    for i in range(context_size):
         context_vector["previous_word_"+str(i+1)] = context_before[i]
         
     #on récupère ensuite le nombre associé au sens pour constuire l'exemple
     gold = int((re.findall("ws_[0-9]",gold_line)[0]).replace("ws_",""))
     
-    '''
-    #on reboucle sur le vecteur pour lemmatizer 
-    for word in context_vector :
-        if word != "instance" :
-            context_vector[word] = nlp(context_vector[word])[0].lemma_
-    '''
     X.append((context_vector,gold))
     
     
